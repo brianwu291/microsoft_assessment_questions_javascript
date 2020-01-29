25d2d9669130f4db656b61b3fecbc580f1fcfa2c
BrianWu <brianwu291@gmail.com>
2020-01-29 15:15:44 +0800

    refs #master, add number 3 question solution

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,4 +1,8 @@
-const { removeThirdRepeatedLetter, findMaxSumInTwoIntDigits } = require('../index')
+const {
+  removeThirdRepeatedLetter,
+  findMaxSumInTwoIntDigits,
+  minMovesOnRemoveThirdConsecutiveLetter,
+} = require('../index')
 
 // removeThirdRepeatedLetter
 it('should return “eedaad” when input “eedaaad”', () => {
@@ -30,4 +34,20 @@ it('should return 102 when input [42, 33, 60]', () => {
 it('should return -1 when input [51, 32, 43]', () => {
   const input =  [51, 32, 43]
   expect(findMaxSumInTwoIntDigits(input)).toBe(-1)
+})
+
+// minMovesOnRemoveThirdConsecutiveLetter
+it('should return 1 when input is "baaaaa" ', () => {
+  const input = 'baaaaa'
+  expect(minMovesOnRemoveThirdConsecutiveLetter(input)).toBe(1)
+})
+
+it('should return 2 when input is "baaabbaabbba" ', () => {
+  const input = 'baaabbaabbba'
+  expect(minMovesOnRemoveThirdConsecutiveLetter(input)).toBe(2)
+})
+
+it('should return 0 when input is "baabab" ', () => {
+  const input = 'baabab'
+  expect(minMovesOnRemoveThirdConsecutiveLetter(input)).toBe(0)
 })
--- index.js
+++ index.js
@@ -75,7 +75,45 @@ function findMaxSumInTwoIntDigits(nums = []) {
   return result
 }
 
+// No.3
+// You are given a string S consisting of N letters ‘a’ and/or ‘b’. In one move, you can swap one letter for the other (‘a’ for ‘b’ or ‘b’ for ‘a’).
+// Write a function solution that, given such a string S, returns the minimum number of moves required to obtain a string containing no instances of three identical consecutive letters.
+
+// Examples:
+// Given S = “baaaaa” , the function should return 1. The string without three identical consecutive letters which can be obtained in one move is “baabaa”.
+// Give S = “baaabbaabbba” , the function should return 2. There are four valid strings obtainable in two moves: for example, “bbaabbaabbaa” .
+// Given S = “baabab” , the function should return 0.
+
+// Write an efficient algorithm for the following assumptions:
+// N is an integer within the range [0..200,000]
+// string S consists only of the characters ‘a’ and/or ‘b’
+
+function minMovesOnRemoveThirdConsecutiveLetter(S = '') {
+  if (S.length < 3) return 0
+  function getMinRemovesTimes(counts = 0) {
+    return Math.floor(counts / 3)
+  }
+  let result = 0
+  const mapObj = {
+    currentLetter: S[0],
+    counts: 1,
+  }
+  for (let i = 1; i < S.length; i += 1) {
+    if (mapObj.currentLetter === S[i]) {
+      mapObj.counts += 1
+    } else {
+      result += getMinRemovesTimes(mapObj.counts)
+      mapObj.currentLetter = S[i]
+      mapObj.counts = 1
+    }
+  }
+  result += getMinRemovesTimes(mapObj.counts)
+  return result
+}
+
+
 module.exports = {
   removeThirdRepeatedLetter,
   findMaxSumInTwoIntDigits,
+  minMovesOnRemoveThirdConsecutiveLetter,
 }
